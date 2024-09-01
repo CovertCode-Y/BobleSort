a7fef2f2eb13b085b0e89af4332f77f6155094fe
YOSIMOSH <yzm62269@gmail.com>
2024-09-01 10:30:18 +0300

    boble sort

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Boble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 55, 11, 50, 77, 69, 70, 20, 9 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (int ar in arr)
+            {
+                Console.Write("  {0}", ar);
+            }
+            Console.Read();
+        }
+
+    }
+}
